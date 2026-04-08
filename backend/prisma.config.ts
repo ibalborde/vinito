f4653f5fa64651eda9abe365f3459d8b6c0ede41
ibalborde <maximiliano_009@hotmail.com>
2026-04-08 15:07:27 -0300

    Fix prisma generate in build using fallback URL in prisma.config.ts

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     path: 'prisma/migrations',
   },
   datasource: {
-    url: process.env['DATABASE_URL'],
+    url: process.env['DATABASE_URL'] ?? 'postgresql://dummy:dummy@localhost:5432/dummy',
   },
   seed: {
     run: 'ts-node prisma/seed.ts',
