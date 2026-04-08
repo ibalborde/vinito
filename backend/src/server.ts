29231cd57778414f853a79965fcbb0d90fff8eee
ibalborde <maximiliano_009@hotmail.com>
2026-04-08 20:38:29 -0300

    tests just run server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,4 @@
-process.on('uncaughtException', (error) => {
-  console.error('Uncaught Exception:', error)
-  process.exit(1)
-})
-
-process.on('unhandledRejection', (reason) => {
-  console.error('Unhandled Rejection:', reason)
-  process.exit(1)
-})
+console.log('>>> EL CONTENEDOR ESTÁ VIVO Y EJECUTANDO SERVER.TS <<<');
 
 import app from './app'
 import { env } from './config/env'
