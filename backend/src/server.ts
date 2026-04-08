3fe0240ae4e0b374cc75e6bc9a1d8ad67221540c
ibalborde <maximiliano_009@hotmail.com>
2026-04-08 17:51:54 -0300

    adds some server logs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,6 +14,10 @@ import { env } from './config/env'
 console.log('Starting server...')
 console.log('PORT:', env.port)
 
+console.log('--- DEBUG DE ENTORNO ---');
+console.log('process.env.PORT:', process.env.PORT);
+console.log('Valor final en env.port:', env.port);
+
 app.listen(env.port, '0.0.0.0', () => {
   console.log(`Server running on port ${env.port}`)
 })
